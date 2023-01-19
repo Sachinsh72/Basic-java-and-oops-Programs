d16ac42e513d68d8d67500f7f35dc96f322577f3
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-01-19 22:41:08 +0530

    basic program

---
+++ ParameterizeArgumentMethod.java
@@ -0,0 +1,11 @@
+public class ParameterizeArgumentMethod {
+    
+    public static void main(String[] args) {
+        System.out.println("Parameterized method starts");
+        System.out.println(sum(20,30));
+    }
+    public static int sum(int a,int b){
+        int sum=a+b;
+        return sum;
+    }
+}
